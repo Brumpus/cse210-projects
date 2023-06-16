6f101290606c4a3141b4d0196460d87917017fc2
Quinton Hogan <quintonius12@gmail.com>
2023-06-16 12:01:04 -0600

    Current journal now clears when new is loaded

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -37,11 +37,14 @@ public class Journal {
     public void LoadFromFile(string file)
     {
         //This replaces all the entries in the _entries list with the ones from a saved file
+        _entries.Clear();
+        
         string[] lines = System.IO.File.ReadAllLines(file);
+
 
         foreach (String diary in lines)
         {
-            string[] parts = diary.Split("|"); // I need to figure out how to separate the values with something other than a comma
+            string[] parts = diary.Split("|"); 
 
             string date = parts[0];
             string prompt = parts[1];
